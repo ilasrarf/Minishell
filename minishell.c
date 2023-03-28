fe3aa8bda84a16a1ffa3e51ea2fb54fbc9a524f2
Alae Eddine En Naas <aen-naas@e1r6p2.1337.ma>
2023-03-28 08:03:46 +0000

    added history

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: aen-naas <aen-naas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 18:09:07 by ilasrarf          #+#    #+#             */
-/*   Updated: 2023/03/28 06:20:11 by aen-naas         ###   ########.fr       */
+/*   Updated: 2023/03/28 07:59:41 by aen-naas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,13 @@ int	main(int ac, char **av, char **env)
 	prs = NULL;
 	(void)ac;
 	(void)av;
+	// int fd = open ("/dev/urandom", O_RDONLY);
+	// dup2(fd, 0);
 	while (1)
 	{
 		str = readline("\e[91mMinishell$ \e[0m");
+		if (str && *str)
+			add_history(str);
 		if (!str)
 			return (0);
 		holder = str;
